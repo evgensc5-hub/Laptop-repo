91daf37c6a201efe76225133acb069ae220bb2c8
Eugen Scutaru <evgensc5@gmail.com>
2026-08-18 19:58:46 +0300

    Initial commit

---
+++ src/main/java/com/laptop/laptop_project/LaptopProjectApplication.java
@@ -0,0 +1,13 @@
+package com.laptop.laptop_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laptop/laptop_project/LaptopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laptop.laptop_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
